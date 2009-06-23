6dc35b33af401c568ef042964b7887765b2df411
Henri Bergius <henri.bergius@iki.fi>
2009-06-23 19:58:45 +0300

    Read configuration

--- qaikubot.py
+++ qaikubot.py
@@ -1,13 +1,17 @@
 from twisted.application import service
 from twisted.words.protocols.jabber.jid import JID
 from twisted.words.xish import domish
+import ConfigParser
 
 from wokkel import client, xmppim, component
 
 from qaiku import BotPresence, BotMessage
 
-myjid = JID("testiapina@jabber.org")
-password = 'salainensana'
+config = ConfigParser.ConfigParser()
+config.read('qaikubot.ini')
+
+myjid = JID(config.get('qaikubot', 'jid'))
+password = config.get('qaikubot', 'password')
 
 application = service.Application('XMPP client')
 xmppClient = client.XMPPClient(myjid, password)
